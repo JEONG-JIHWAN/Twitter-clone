1ab12c098c2d978334b15d1cab7cb1678b1e5f29
JEONG-JIHWAN <jjw981210@gmail.com>
2021-01-03 03:19:59 +0900

    getting tweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,22 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {dbService} from "fbase"
 
 const Home = () => {
     const [jweet, setJweet] = useState("")
+    const [jweets, setJweets] = useState([]);
+    const getNweets = async()=>{
+        const dbJweets = await dbService.collection("jweets").get();
+        dbJweets.forEach((document)=> {
+            const jweetObject = {
+                ...document.data(),
+                id: document.id,
+            }
+            setJweets(prev=>[jweetObject,...prev])
+        });
+    }
+    useEffect(() => {
+        getNweets();
+    },[])
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("jweets").add({
@@ -15,11 +29,20 @@ const Home = () => {
         const {target:{value}} = event;
         setJweet(value);
     }
+    console.log(jweets);
     return (
+        <>
         <form onSubmit={onSubmit}>
             <input value={jweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
             <input type="submit" value="Jweet"/>
         </form>
+        <div>
+            {jweets.map(jweet => <div key={jweet.id}>
+                <h4>{jweet.jweet}</h4>
+                </div>)}
+        </div>
+
+        </>
     )
 
 }
